0174d4926c8f6865c7b296d3c7f8c41aecba32d4
reqx182 <96117929+reqx182@users.noreply.github.com>
2022-08-10 19:39:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,163 @@
 import logging
+import pytube
+import os
 
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+#os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
+# from moviepy.editor import AudioFileClip
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters, ConversationHandler
 
-# Enable logging
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
 
+KEYBOARD, BUTTONS, LINK = range(3)
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Введите ссылку ')
+    return KEYBOARD
+
+
+def keyboard(update: Update, context: CallbackContext):
+    url = update.message.text
+    """Выводит клавиатуру Загрузить видео | Загрузить аудио"""
+    keyboard = [[
+        InlineKeyboardButton("video", callback_data='video'),
+        InlineKeyboardButton("audio", callback_data='audio')],
+        [
+            InlineKeyboardButton("cancel", callback_data='cancel')
+        ]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(url, reply_markup=reply_markup)
+    return BUTTONS
+
+
+def link_check(link) -> bool:
+    try:
+        pytube.YouTube(link)
+        return True
+    except:
+        print("Неправильная ссылка")
+        return False
+
+
+def delet_syms(title: str) -> str:
+    """Удаляет лишние символы из названия файла во избежании ошибки при сохранении"""
+    wrong_syms = '|\\/_-!"№;%:?*+='
+    title = title.split()
+    for index in range(len(title)):
+        if title[index] in wrong_syms:
+            title[index] = ''
+    title = "".join(title)
+    return title
+
+
+def video(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    link = update.callback_query['message']['text']
+    if not link_check(link):
+        update.callback_query.bot.send_message(chat_id, 'Неправильная ссылка. Введите ссылку ещё раз.')
+        return KEYBOARD
+
+    update.callback_query.bot.send_message(chat_id, 'Подождите, видео скачивается.')
+    yt = pytube.YouTube(link)
+    stream = yt.streams.get_highest_resolution()
+    title = f'{yt.title}.mp4'
+    filename = delet_syms(title)
+    stream.download(filename=filename)
+    file = open(filename, 'rb')
+    update.callback_query.bot.send_video(chat_id, file, timeout = 100)
+    print(os.getcwd())
+    # os.remove(f'{os.getcwd()}/{filename}')
+    print('video')
+
+    return KEYBOARD
+
+
+def audio(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    link = update.callback_query['message']['text']
+    if not link_check(link):
+        update.callback_query.bot.send_message(chat_id, 'Неправильная ссылка. Введите ссылку ещё раз.')
+        return KEYBOARD
+
+    update.callback_query.bot.send_message(chat_id, 'Подождите, аудио скачивается.')
+    yt = pytube.YouTube(link)
+    stream = yt.streams.get_highest_resolution()
+    filename = yt.title
+    filename = delet_syms(filename)
+    stream.download(filename=filename)
+    convert_to_mp3(filename + '.mp4', filename + '.mp3')
+    file = open(filename, 'rb')
+    update.callback_query.bot.send_audio(chat_id, file, timeout = 100)
+    file.close()
+    os.remove(f'{os.getcwd()}/{filename}')
+    print('audio')
+
+    return KEYBOARD
+
+
+def convert_to_mp3(input_mp4_file, output_name_file='без названия.mp3', delete_input_file=True):
+	pass
+#     in_audio = AudioFileClip(input_mp4_file)
+#     in_audio.write_audiofile(filename=output_name_file)
+#     in_audio.close()
+#     print('конвертация выполнена')
+#     if delete_input_file:
+#         os.remove(input_mp4_file)
+
+
+def cancel(update: Update, context: CallbackContext):
+    update.message.reply_text('Введите новую ссылку')
+    return KEYBOARD
+
+
+def exit(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(
+        'Bye! I hope we can talk again some day.'
     )
+    return ConversationHandler.END
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+def send_audio(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    url = open('AFRAID.mp4', 'rb')
+    update.message.bot.send_audio(chat_id, url)
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
-def main() -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    updater = Updater("5386827464:AAGLD9D-ypRRQJmc-LTB82EUW7VDVRmTKt0")
-
-    # Get the dispatcher to register handlers
+def main():
+    TOKEN = '5386827464:AAGLD9D-ypRRQJmc-LTB82EUW7VDVRmTKt0'
+    updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            KEYBOARD: [
+                MessageHandler(Filters.text, keyboard)
+            ],
+            BUTTONS: [
+                CallbackQueryHandler(video, pattern='video'),
+                CallbackQueryHandler(audio, pattern='audio'),
+                CallbackQueryHandler(cancel, pattern='cancel')
+            ]
+        },
+        fallbacks=[CommandHandler('exit', exit)])
 
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+    # filters.TEXT & (filters.Entity(URL) | filters.Entity(TEXT_LINK)) ?????
+    # filters.TEXT & (~ filters.FORWARDED)
+    # Filters.regex('^(Boy|Girl|Other)$')
 
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
-
-    # Start the Bot
+    dispatcher.add_handler(CommandHandler('send', send_audio))
+    dispatcher.add_handler(conv_handler)
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
 if __name__ == '__main__':
     main()
+
